03988ca2d604cd81a30478bc2dc301c8cfdbcf12
Johannes Jahn <johannes.jahn@outlook.com>
2022-12-01 09:23:42 +0100

    Add solution 1

--- 01/solution.py
+++ 01/solution.py
@@ -4,10 +4,42 @@ def readFile():
     return lines
 
 
-def solve(lines: list[str]):
-    return 7
+def solve1(lines: list[str]):
+    print(lines)
+
+    res = list()
+    currIndex = 0
+    res.append(0)
+
+    for i in range(len(lines)):
+        if lines[i] == '':
+            currIndex += 1
+            res.append(0)
+            continue
+        res[currIndex] += int(lines[i])
+
+    res.sort()
+    print(max(res))
+
+def solve2(lines: list[str]):
+    print(lines)
+
+    res = list()
+    currIndex = 0
+    res.append(0)
+
+    for i in range(len(lines)):
+        if lines[i] == '':
+            currIndex += 1
+            res.append(0)
+            continue
+        res[currIndex] += int(lines[i])
+
+    res.sort()
+    print(res[-1] + res[-2] + res[-3])
+
 
 
 if __name__ == '__main__':
     lines = readFile()
-    solve(lines)
+    solve2(lines)

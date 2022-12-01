5a4a9d2ba1ad9d47ef273538f75c3aa5ac813a83
Johannes Jahn <johannes.jahn@outlook.com>
2022-12-01 09:24:54 +0100

    Add solution 2 template

---
+++ 02/solution.py
@@ -0,0 +1,17 @@
+def readFile():
+    text_file = open("./02/input.txt", "r")
+    lines = text_file.read().split('\n')
+    return lines
+
+
+def solve1(lines: list[str]):
+    print(lines)
+
+def solve2(lines: list[str]):
+    pass
+
+
+
+if __name__ == '__main__':
+    lines = readFile()
+    solve2(lines)

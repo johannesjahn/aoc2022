1be259b4d35f66211db3db7acb8485200c0cafbb
Johannes Jahn <johannes.jahn@outlook.com>
2022-12-02 11:12:29 +0100

    Add solution for day 2

--- 02/solution.py
+++ 02/solution.py
@@ -5,11 +5,47 @@ def readFile():
 
 
 def solve1(lines: list[str]):
-    print(lines)
+
+    score = 0
+
+    for line in lines:
+        game = line.split(' ')
+        a = ord(game[0]) - 65
+        b = ord(game[1]) - 88
+        score += gameWinner(a, b) + b + 1
+
+    return score
 
 
 def solve2(lines: list[str]):
-    pass
+
+    score = 0
+
+    for line in lines:
+        game = line.split(' ')
+        a = ord(game[0]) - 65
+        b = rightMove(a, game[1])
+        score += gameWinner(a, b) + b + 1
+
+    print(score)
+
+
+def rightMove(va, b):
+    vb = ord(b) - 89
+
+    res = (va + vb) % 3
+    return res
+
+
+def gameWinner(a, b):
+    res = b - a
+
+    if res > 1:
+        res -= 3
+    elif res < -1:
+        res += 3
+
+    return (res + 1) * 3
 
 
 if __name__ == '__main__':

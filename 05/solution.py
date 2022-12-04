81da6a53628433dedfa955bc1f9eaf9785d85d36
Johannes Jahn <johannes.jahn@outlook.com>
2022-12-04 12:15:06 +0100

    Add 05 boilerplate

---
+++ 05/solution.py
@@ -0,0 +1,18 @@
+def readFile():
+    text_file = open("./05/input.txt", "r")
+    lines = text_file.read().split('\n')
+    return lines
+
+
+def solve1(lines: list[str]):
+    pass
+
+
+def solve2(lines: list[str]):
+    pass
+
+
+if __name__ == '__main__':
+    lines = readFile()
+    print(solve1(lines))
+    print(solve2(lines))

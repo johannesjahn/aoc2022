977b4a669894b1b8bb22b292febefa342e78d5c8
Johannes Jahn <johannes.jahn@outlook.com>
2022-12-07 01:54:54 +0100

    Add day6 solution

--- 06/solution.py
+++ 06/solution.py
@@ -1,17 +1,24 @@
+from collections import Counter
 
 
 def readFile():
     text_file = open("./06/input.txt", "r")
-    lines = text_file.read().split('\n')
+    lines = text_file.read()
     return lines
 
 
 def solve1(lines: list[str]):
-    pass
+    for i in range(len(lines) - 3):
+        buffer = lines[i: i + 4]
+        if len(Counter(buffer)) == 4:
+            return i + 4
 
 
 def solve2(lines: list[str]):
-    pass
+    for i in range(len(lines) - 13):
+        buffer = lines[i: i + 14]
+        if len(Counter(buffer)) == 14:
+            return i + 14
 
 
 if __name__ == '__main__':

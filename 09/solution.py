32fb34d8c065c706718e48755fe290969a0937f1
Johannes Jahn <johannes.jahn@outlook.com>
2022-12-08 20:09:31 +0100

    Add day 09 boilerplate

---
+++ 09/solution.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+def readFile():
+    text_file = open("./09/input.txt", "r")
+    lines = text_file.read().split('\n')
+    return lines
+
+
+def solve1(lines: list[str]):
+    pass
+
+
+def solve2(lines: list[str]):
+    pass
+
+
+if __name__ == '__main__':
+    lines = readFile()
+    print(solve1(lines))
+    print(solve2(lines))
